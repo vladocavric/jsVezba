2a8b546c39d9b6739746d6a1f2e1dadee5e72e0b
Vlado Cavric <vladocavric@gmail.com>
2019-03-17 14:41:48 +0100

    Introduction to JavaScript

---
+++ Introduction to JavaScript/1.js
@@ -0,0 +1,7 @@
+var fName = prompt("kako se zoves")
+var lName = prompt("kako se prezivas")
+var fullName = fName + " " + lName
+var age = prompt("koliko imas godina")
+alert("nice to meet you, " + fullName)
+alert("are you realy " + age + "?" )
+console.log("again nice to meet you " + fullName + " who have " + age)
---
+++ Introduction to JavaScript/ageCalc.js
@@ -0,0 +1,5 @@
+
+var age = prompt("koliko imas godina")
+var leapYears = Math.floor(age/4)
+var days = age * 365 + leapYears
+alert("star si odprilike " + days + " dana" )
